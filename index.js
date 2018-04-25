515c3d5bdd3918aa95d3f421bd28f755cebabf50
Matias Candelieri <matias.candelieri@personal.com.ar>
2018-04-25 12:59:34 -0300

    Cambio en index.js para tomar archivo de configuracion

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
 
-var fs = require('fs');
+var fs = require('fs'),
+    xml2js = require('xml2js');
 
 
 router.use(bodyParser.json());
@@ -11,14 +12,28 @@ router.use(bodyParser.json());
 
 router.route('/getFile').get(function (req, response) {
 
-    var file = process.env.CONFIGXML;
+    var dataXml = process.env.CONFIGXML;
 
-    //var parser = new xml2js.Parser();
-    fs.readFile('genericQueryXMLall.xml', (err, data) => {
-      if (err) throw response.send('No hay archivo o puede que su configuracion sea erronea');
-      response.download(data);
+    var parser = new xml2js.Parser();
+    fs.readFile( dataXml, function(err, data) {
+        if(err)
+        {
+            response.send('No hay archivo o puede que su configuracion sea erronea');
+        }
+        else {
+            parser.parseString(data, function (err, result) {
+                var file = result.route;
+
+                response.download(file); // Set disposition and send it.
+
+            });
+        }
     });
-    
+
+
+
+
+
 });
 
 module.exports = router;
