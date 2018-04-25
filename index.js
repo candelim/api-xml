126d943b8d6e842ff464daf0e430b80e52bfa7f5
Matias <matias.candelieri@personal.com.ar>
2018-04-25 12:36:27 -0300

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ router.route('/getFile').get(function (req, response) {
     var file = process.env.CONFIGXML;
 
     //var parser = new xml2js.Parser();
-    fs.readFile('./genericQueryXMLall.xml', (err, data) => {
+    fs.readFile('genericQueryXMLall.xml', (err, data) => {
       if (err) throw response.send('No hay archivo o puede que su configuracion sea erronea');
       response.download(data);
     });
