bd5eca6bbf72c1da104c8de532807a68b1c51d18
Matias <matias.candelieri@personal.com.ar>
2018-05-10 23:31:22 -0300

    Modifico todos los route, agregando el __dirname

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ var zlib = require('zlib')
 
 router.use(bodyParser.json());
 /* GET users listing. */
+//CONFIGXML='genericQueryXMLall.xml' FILECUTOVERAVANCE='cutover_fan_avance.csv' FILECUTOVERSTATUS='cutover_fan_status.csv'
 
 router.route('/getFile').get(function (req, response) {
-    console.log(__dirname + '/' + process.env.CONFIGXML);
-
+    
     fs.readFile( __dirname + '/' + process.env.CONFIGXML, function(err, data) {
         if(err)
         {
@@ -24,13 +24,12 @@ router.route('/getFile').get(function (req, response) {
 
         }
     });
-    //CONFIGXML='./genericQueryXMLall.xml' FILECUTOVERAVANCE='./cutover_fan_avance.csv' FILECUTOVERSTATUS='./cutover_fan_status.csv'
 
 });
 
 router.route('/getFileCutoverAvance').get(function (req, response) {
 
-    fs.readFile( process.env.FILECUTOVERAVANCE, function(err, data) {
+    fs.readFile( __dirname + '/' + process.env.FILECUTOVERAVANCE, function(err, data) {
         if(err)
         {
             response.send('No hay archivo o puede que su configuracion sea erronea');
@@ -51,7 +50,7 @@ router.route('/getFileCutoverStatus').get(function (req, response) {
 
     //response.chunkedEncoding = true
 
-    fs.readFile( process.env.FILECUTOVERSTATUS, function(err, data) {
+    fs.readFile( __dirname + '/' + process.env.FILECUTOVERSTATUS, function(err, data) {
         if(err)
         {
             response.send('No hay archivo o puede que su configuracion sea erronea');
